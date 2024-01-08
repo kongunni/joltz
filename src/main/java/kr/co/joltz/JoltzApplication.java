1d8f0fa598eb7415657630709223fe51c1309214
joltzgigi <kongunni821@gmail.com>
2024-01-08 21:31:56 +0900

    [0108] intellij test commit

---
+++ src/main/java/kr/co/joltz/JoltzApplication.java
@@ -0,0 +1,13 @@
+package kr.co.joltz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoltzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/joltz/JoltzApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.joltz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoltzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
